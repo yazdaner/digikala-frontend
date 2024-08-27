3b2a5f88180443688b4407a4da2fe1c3aa731134
yazdan <yazdaner8@gmail.com>
2024-08-27 23:11:37 +0330

    footer desktop 1.3.0

--- components/shop/Footer/desktop/DesktopFooter.vue
+++ components/shop/Footer/desktop/DesktopFooter.vue
@@ -54,32 +54,32 @@
                 <h5 class="mb-3">با دیجی‌کالا</h5>
                 <ul class="none-list-style p-0">
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             اتاق خبر دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             فروش در دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             فرصت‌های شغلی
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             گزارش تخلف در دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             تماس با دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             درباره دیجی‌کالا
                         </nuxt-link>
                     </li>
@@ -89,32 +89,32 @@
                 <h5 class="mb-3">با دیجی‌کالا</h5>
                 <ul class="none-list-style p-0">
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             اتاق خبر دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             فروش در دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             فرصت‌های شغلی
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             گزارش تخلف در دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             تماس با دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             درباره دیجی‌کالا
                         </nuxt-link>
                     </li>
@@ -124,32 +124,32 @@
                 <h5 class="mb-3">با دیجی‌کالا</h5>
                 <ul class="none-list-style p-0">
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             اتاق خبر دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             فروش در دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             فرصت‌های شغلی
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             گزارش تخلف در دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             تماس با دیجی‌کالا
                         </nuxt-link>
                     </li>
                     <li class="mb-3 fs-15">
-                        <nuxt-link class=" color-gray" to="/">
+                        <nuxt-link class="color-gray" to="/">
                             درباره دیجی‌کالا
                         </nuxt-link>
                     </li>
@@ -157,7 +157,75 @@
             </div>
             <div class="col-md-3">
                 <h5 class="mb-3">همراه ما باشید!</h5>
-                
+                <nuxt-link to="/">
+                    <fa-icon
+                        :icon="['fab', 'instagram']"
+                        class="social-icon-footer"
+                    />
+                </nuxt-link>
+                <nuxt-link to="/">
+                    <fa-icon
+                        :icon="['fab', 'twitter']"
+                        class="social-icon-footer"
+                    />
+                </nuxt-link>
+                <nuxt-link to="/">
+                    <fa-icon
+                        :icon="['fab', 'linkedin']"
+                        class="social-icon-footer"
+                    />
+                </nuxt-link>
+                <nuxt-link to="/">
+                    <fa-icon
+                        :icon="['fab', 'fa-youtube']"
+                        class="social-icon-footer"
+                    />
+                </nuxt-link>
+            </div>
+        </div>
+        <div class="row mt-5 border-top">
+            <div class="col-md-8">
+                <h5 class="color-gray-3">
+                    فروشگاه اینترنتی دیجی‌کالا، بررسی، انتخاب و خرید آنلاین
+                </h5>
+                <div class="mt-3">
+                    <p class="fs-13 color-gray-2 line-h-2">
+                        یک خرید اینترنتی مطمئن، نیازمند فروشگاهی است که بتواند
+                        کالاهایی متنوع، باکیفیت و دارای قیمت مناسب را در مدت
+                        زمان ی کوتاه به دست مشتریان خود برساند و ضمانت بازگشت
+                        کالا هم داشته باشد؛ ویژگی‌هایی که فروشگاه اینترنتی
+                        دیجی‌کالا سال‌هاست بر روی آن‌ها کار کرده و توانسته از
+                        این طریق مشتریان ثابت خود را داشته باشد. یکی از مهم‌ترین
+                        دغدغه‌های کاربران دیجی‌کالا یا هر فروشگاه‌ اینترنتی
+                        دیگری، این است که کالای خریداری شده چه زمانی به دستشان
+                        می‌رسد. دیجی‌کالا شیوه‌های مختلفی از ارسال را متناسب با
+                        فروشنده کالا،‌ مقصد کالا و همچنین نوع کالا در اختیار
+                        کاربران خود قرار می‌دهد. هر یک از روش های ارسال دیجی
+                        کالا شرایط و ویژگی‌های خاص خود را دارند که ممکن است گاهی
+                        برای کاربران جدید، مبهم و پیچیده به نظر برسند. برای
+                        آگاهی بیشتر مشتریان از خدمات دیجی‌کالا، این فروشگاه
+                        اینترنتی در بخشی از وب‌سایت خود راهنمای کاملی از
+                        شیوه‌‌های ارسال را به صورت ساده و به دور از پیچیدگی،
+                        قرار داده است که شامل 8 نوع ارسال کالا به روش‌های زیر
+                        است: ارسال امروز، دریافت حضوری، دریافت از گنجه، ارسال
+                        توسط فروشنده، ارسال عادی، ارسال کالاهای بزرگ، سنگین و
+                        فوق سنگین، ارسال سریع سوپرمارکتی، ارسال با پست کدام
+                        محصولات در دیجی‌کالا قابل سفارش هستند؟
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="d-flex">
+                    <div class="certificate-footer">
+                        <img src="/public/images/footer/kasbokar.webp" alt="kasbokar">
+                    </div>
+                    <div class="certificate-footer">
+                        <img src="/public/images/footer/rezi.webp" alt="rezi">
+                    </div>
+                    <div class="certificate-footer">
+                        <img src="/public/images/footer/image-placeholder.svg" alt="image-placeholder">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- plugins/fontawesome.client.js
+++ plugins/fontawesome.client.js
@@ -2,10 +2,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
-import { faPaypal, faPhp, faJs } from "@fortawesome/free-brands-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas);
 library.add(far);
-library.add(faPaypal, faPhp, faJs);
+library.add(fab);
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component("fa-icon", FontAwesomeIcon);
 });
