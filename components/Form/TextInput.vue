6c339778ec0b5ebba12a8e1cf64a2ffe9e7cef3d
yazdan <yazdaner8@gmail.com>
2024-08-29 22:14:31 +0330

    form component 1.0.0

---
+++ components/Form/TextInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="input-box" :class="disable == true ? 'disable' : ''">
+        <label v-if="hideLabel != true"
+            :style="{top:top}"
+        >
+            {{ label }}
+        </label>
+        <div class="input-div" :class="[error ? 'error-border' : '']">
+            <input class="form-control"
+                :disabled="disable"
+            >
+        </div>
+        <div v-if="error" class="has-error">
+            {{ error }}
+        </div>
+    </div>
+</template>
+<script setup>
+    defineProps({
+        disable : {
+            type : Boolean
+        },
+        label:{
+            type : String
+        },
+        hideLabel:{
+            type : Boolean
+        }
+    })
+    const top = ref('12px');
+    const error = ref(false);
+    const value = ref('');
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center">
+        <div style="width: 300px;margin: 20px;">
+            <FormTextInput label="نام کاربری" disable />
+        </div>
+    </div>
+</template>
+<script setup>
+definePageMeta({
+    layout: false,
+});
+</script>
