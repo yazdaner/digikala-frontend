276c92ff0ab9677bf6e97d4c9012ff864b3c2678
yazdan <yazdaner8@gmail.com>
2024-09-06 13:17:05 +0330

    admin panel 1.4.0

--- components/admin/Sidebar.vue
+++ components/admin/Sidebar.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <div
-            v-if="theme == 'desktop'"
-            :style="{ width: toggled == true ? '70px' : '260px' }"
-        >
+        <div v-if="theme == 'desktop'">
             <div class="sidebar" :class="[toggled == true ? 'toggled' : '']">
                 <div class="title">
                     <h5 class="">پنل مدیریت</h5>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="mt-60">
        <p>داشبود</p>
     </div>
 </template>
