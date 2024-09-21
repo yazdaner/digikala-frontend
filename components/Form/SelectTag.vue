aafdf42681085031e754446880c53e153245c107
yazdan <yazdaner8@gmail.com>
2024-09-21 20:31:23 +0330

    form component 1.6.1

--- components/Form/SelectTag.vue
+++ components/Form/SelectTag.vue
@@ -2,12 +2,40 @@
     <div class="select-tag relative" :class="{ disable: disable }">
         <div @click="showBox" class="tag" :class="{ 'error-border': error }">
             <div>
-                <label v-if="hideLabel != true" :style="{ top: top }">
+                <label :style="{ top: top }" v-if="text == null">
                     {{ label }}
                 </label>
-                <img :src="icon" v-if="icon != null" class="box-16">
-                <span></span>
+                <img :src="icon" v-if="icon != null" class="box-16" />
+                <span>
+                    {{ $replaceEnNumber(text) }}
+                </span>
             </div>
+            <div v-if="showList && disable == false" class="item-list w-100">
+                <div class="my-3" v-if="items.length > 10">
+                    <FormTextInput
+                        placeholder="جستجو"
+                        hide-label
+                        v-model="searchText"
+                        class="search-input"
+                    />
+                </div>
+                <ul class="none-list-style">
+                    <template v-for="(item, key) in items" :key="key">
+                        <li
+                            @click="selectItem(item)"
+                            :class="{ active: model == item[itemValue] }"
+                        >
+                            <div v-if="item['icon'] !== undefined">
+                                <img :src="item['icon']" class="box-16" />
+                            </div>
+                            <span>
+                                {{ $replaceEnNumber(item[itemText]) }}
+                            </span>
+                        </li>
+                    </template>
+                </ul>
+            </div>
+            <fa-icon ref="selectIcon" :icon="['fas', 'angle-down']" />
         </div>
         <div v-if="error" class="has-error">
             {{ error }}
@@ -50,7 +78,12 @@ const model = defineModel();
 const { top, error, validateInput, click } = input(props, model);
 const showList = ref(false);
 const icon = ref(null);
-function showBox() {}
+const text = ref(null);
+const selectIcon = ref(null);
+const searchText = ref(null);
+function showBox() {
+    showList.value = !showList.value;
+}
 function validate() {
     return validateInput(props, model.value);
 }
--- pages/test.vue
+++ pages/test.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
         <div style="width: 300px; margin: 20px">
-            <FormComponent action="shop/categories-data?ids=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16" :result="showResponse">
-                <FormTextInput label="نام کاربری" :rules="['required']" name="username" id="username"/>
-                <FormTextInput label="xxxxxxxxx" :rules="['required']" name="xxxxxxxxx" id="xxxxxxxxx"/>
-                <FormTextInput label="fff" :rules="['required']" name="fff" id="fff"/>
-                <FormPasswordInput label="رمز" rules="required" name="password" id="password"/>
+            <FormComponent
+                action="shop/categories-data?ids=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16"
+                :result="showResponse"
+            >
+                <FormTextInput
+                    label="نام کاربری"
+                    :rules="['required']"
+                    name="username"
+                    id="username"
+                />
+                <FormTextInput
+                    label="xxxxxxxxx"
+                    :rules="['required']"
+                    name="xxxxxxxxx"
+                    id="xxxxxxxxx"
+                />
+                <FormTextInput
+                    label="fff"
+                    :rules="['required']"
+                    name="fff"
+                    id="fff"
+                />
+                <FormSelectTag
+                    :items
+                    item-text="name"
+                    item-value="id"
+                    label="استان"
+                     
+                />
+                <FormPasswordInput
+                    label="رمز"
+                    rules="required"
+                    name="password"
+                    id="password"
+                />
                 <FormButton design="btn-success">ورود</FormButton>
             </FormComponent>
         </div>
@@ -17,8 +47,42 @@ definePageMeta({
 });
 const username = ref(null);
 
-function showResponse(response)
-{
+function showResponse(response) {
     console.log(response);
 }
+
+const items = [
+    { id: 1, name: "Iowa", deleted_at: null },
+    { id: 2, name: "South Dakota", deleted_at: null },
+    { id: 3, name: "Tennessee", deleted_at: null },
+    { id: 4, name: "Pennsylvania", deleted_at: null },
+    { id: 5, name: "California", deleted_at: null },
+    { id: 6, name: "Nebraska", deleted_at: null },
+    { id: 7, name: "Kentucky", deleted_at: null },
+    { id: 8, name: "okZU4d77AG", deleted_at: null },
+    { id: 10, name: "Tennessee", deleted_at: null },
+    { id: 11, name: "Tennessee", deleted_at: null },
+    { id: 12, name: "California", deleted_at: null },
+    { id: 13, name: "Missouri", deleted_at: null },
+    { id: 14, name: "Nevada", deleted_at: null },
+    { id: 15, name: "Iowa", deleted_at: null },
+    { id: 16, name: "Missouri", deleted_at: null },
+    { id: 17, name: "Kansas", deleted_at: null },
+    { id: 18, name: "Wisconsin", deleted_at: null },
+    { id: 19, name: "LBbUJnJbr9", deleted_at: null },
+    { id: 21, name: "Utah", deleted_at: null },
+    { id: 22, name: "Arizona", deleted_at: null },
+    { id: 23, name: "Hawaii", deleted_at: null },
+    { id: 24, name: "Louisiana", deleted_at: null },
+    { id: 25, name: "Vermont", deleted_at: null },
+    { id: 26, name: "New York", deleted_at: null },
+    { id: 27, name: "Alaska", deleted_at: null },
+    { id: 28, name: "North Dakota", deleted_at: null },
+    { id: 29, name: "Rhode Island", deleted_at: null },
+    { id: 30, name: "South Carolina", deleted_at: null },
+    { id: 31, name: "oS8gr8pz4O", deleted_at: null },
+    { id: 33, name: "Indiana", deleted_at: null },
+    { id: 34, name: "New Mexico", deleted_at: null },
+    { id: 35, name: "Ohio", deleted_at: null },
+];
 </script>
